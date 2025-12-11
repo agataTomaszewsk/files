f39df89b99afdb4603843e823a040c777a482515
Agata Tomaszewska <152228883+agataTomaszewsk@users.noreply.github.com>
2025-12-11 10:36:34 +0100

    Update Portfolio.h

--- Portfolio.h
+++ Portfolio.h
@@ -5,20 +5,17 @@
 
 class Portfolio {
 private:
-    // Wektor wskaźników do klasy bazowej Instrument
     std::vector<Instrument*> instruments;
 
 public:
     Portfolio();
-    ~Portfolio(); // Destruktor zwalniający pamięć
+    ~Portfolio(); 
 
-    // Blokujemy kopiowanie, aby uniknąć problemów z podwójnym usuwaniem wskaźników
     Portfolio(const Portfolio&) = delete;
     Portfolio& operator=(const Portfolio&) = delete;
 
-    // Metody wymagane przez zadanie
     void addInstrument(Instrument* inst);
-    void removeInstrument(const std::string& symbol); // Usuwa instrument po symbolu
+    void removeInstrument(const std::string& symbol); 
     double totalValue() const;
     void display() const;
 };
