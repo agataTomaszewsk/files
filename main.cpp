cc75f5ce8a29f9f249474d6420ba2c0747ce1cce
Agata Tomaszewska <152228883+agataTomaszewsk@users.noreply.github.com>
2025-12-11 13:18:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void testSignalGenerator() {
     // Testujemy wielokrotnie, aby upewnić się, że wynik to zawsze -1 lub 1
     for(int i = 0; i < 10; i++) {
         int sig = gen.generateSignal(100.0);
-        // Sprawdzamy czy sygnał jest poprawny (musi być 1 albo -1)
+        // Sprawdzamy czy sygnał jest poprawny 
         bool isValid = (sig == 1 || sig == -1);
         ASSERT(isValid);
     }
@@ -20,29 +20,25 @@ void testSignalGenerator() {
 
 void testTradingStrategy() {
     INFO("Testowanie TradingStrategy");
-
-    // Przygotowanie danych
+    
     std::vector<double> marketPrices = {100.5, 101.0, 99.5, 102.0};
     TradingStrategy strategy(marketPrices);
 
-    // Sprawdzenie gettera
     ASSERT(strategy.getPrices().size() == 4);
     ASSERT_APPROX_EQUAL(strategy.getPrices()[0], 100.5, 0.01);
 
-    // Wykonanie strategii
+
     strategy.execute();
 
-    // Sprawdzenie czy wygenerowano tyle sygnałów ile było cen
     std::vector<int> results = strategy.getSignals();
     ASSERT(results.size() == 4);
 
-    // Sprawdzenie czy każdy wynik jest poprawny (-1 lub 1)
     for(int sig : results) {
         bool isValid = (sig == 1 || sig == -1);
         ASSERT(isValid);
     }
 
-    // Test Settera
+
     std::vector<double> newPrices = {50.0, 60.0};
     strategy.setPrices(newPrices);
     strategy.execute();
@@ -51,7 +47,7 @@ void testTradingStrategy() {
 }
 
 int main() {
-    // Inicjalizacja generatora liczb losowych
+
     std::srand(std::time(0));
     setDebugEnabled(true);
 
