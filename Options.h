cc692ab10b74143f3bd41719a3f4bc709e241c8d
Agata Tomaszewska <152228883+agataTomaszewsk@users.noreply.github.com>
2025-12-11 10:33:51 +0100

    Update Options.h

--- Options.h
+++ Options.h
@@ -10,14 +10,12 @@ protected:
 public:
     Option(double strikePrice, double maturityTime);
     virtual ~Option();
-
-    // 1. Wersja podstawowa (czysto wirtualna)
+    // Wersja podstawowa
     virtual double payoff(double spotPrice) const = 0;
 
-    // 2. Wersja przeciążona (z dyskontowaniem)
+    // Wersja z dyskontowaniem
     double payoff(double spotPrice, double discountFactor) const;
 
-    // Getters / Setters
     double getStrike() const;
     void setStrike(double k);
 
@@ -28,20 +26,17 @@ public:
 // Opcja Europejska Kupna (Call)
 class EuropeanCall : public Option {
 public:
-    // --- KLUCZOWA POPRAWKA ---
-    // Przywracamy widoczność metody payoff(double, double) z klasy bazowej
+
     using Option::payoff; 
 
     EuropeanCall(double strikePrice, double maturityTime);
     
-    // Implementacja wersji z jednym argumentem
     double payoff(double spotPrice) const override;
 };
 
 // Opcja Europejska Sprzedaży (Put)
 class EuropeanPut : public Option {
 public:
-    // --- KLUCZOWA POPRAWKA ---
     using Option::payoff;
 
     EuropeanPut(double strikePrice, double maturityTime);
